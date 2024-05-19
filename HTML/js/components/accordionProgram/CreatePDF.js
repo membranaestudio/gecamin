0c3e153f8fe6397ba9976654ee5d54176a455fd8
Andrés <andres@membranaestudio.cl>
2024-05-19 15:08:37 -0400

    varios

--- HTML/js/components/accordionProgram/CreatePDF.js
+++ HTML/js/components/accordionProgram/CreatePDF.js
@@ -1,39 +1,41 @@
 export default class CreatePDF extends BaseComponent {
-    constructor({
-        name,
-        loadInnerComponents,
-        parent,
-        element
-    }) {
-        super({
-            name,
-            loadInnerComponents,
-            parent,
-            element
-        });
+    constructor({ name, loadInnerComponents, parent, element }) {
+        super({ name, loadInnerComponents, parent, element });
         this.setup();
     }
 
-    init() {
+    async init() {
         this.DownloadPDF();
         this.applyPDFStyles();
     }
 
-    // Botón de descarga para iniciar la creación del PDF
     DownloadPDF() {
         const downloadButton = document.getElementById('downloadButton');
-        downloadButton.addEventListener('click', () => {
-            this.CreatePDF();
+        const pdfMessage = document.querySelector('.pdfMessage');
+        pdfMessage.style.display = 'none';
+        downloadButton.addEventListener('click', async () => {
+            console.log('Inicio del proceso de descarga del PDF');
+            downloadButton.style.display = 'none';
+            pdfMessage.style.display = 'block';
+            try {
+                await this.CreatePDF();
+                console.log('PDF generado y listo para descargar');
+            } catch (err) {
+                console.error('Error al generar el PDF:', err);
+            } finally {
+                downloadButton.style.display = 'flex';
+                pdfMessage.style.display = 'none';
+                console.log('Proceso de descarga del PDF completado');
+            }
         });
     }
 
-    // Crea el PDF y maneja la lógica
-    CreatePDF() {
+    async CreatePDF() {
         const pdfWidth = 1380;
-        const pdfHeight = 1600;
+        const pdfHeight = 1800;
         const headerHeight = 174;
         const spaceBelowHeader = 80;
-        const footerSpace = 80; // Espacio para el footer
+        const footerSpace = 80;
         const pdf = new jspdf.jsPDF({
             orientation: 'portrait',
             unit: 'px',
@@ -43,62 +45,56 @@ export default class CreatePDF extends BaseComponent {
         const containers = document.querySelectorAll('#content-to-capture');
         console.log('Contenedores encontrados:', containers.length);
         let currentPageHeight = headerHeight + spaceBelowHeader;
-        const pageHeight = pdf.internal.pageSize.height - footerSpace; // Ajustar la altura de la página para el footer
+        const pageHeight = pdf.internal.pageSize.height - footerSpace;
         const captureWidth = 1260;
-        let promiseChain = Promise.resolve();
         const pdfCloneContainer = document.querySelector('.pdf-clone');
 
-        // Inicialmente añade el encabezado
-        promiseChain = promiseChain.then(() => this.addHeader(pdf, pdfWidth, headerHeight));
+        await this.addHeader(pdf, pdfWidth, headerHeight);
         this.applyPDFStyles();
 
-        containers.forEach((container, index) => {
-            Array.from(container.querySelectorAll('.pdf-item')).forEach((heightItem, idx) => {
-                promiseChain = promiseChain.then(() => {
-                    return new Promise((resolveRow) => {
-                        const clone = heightItem.cloneNode(true);
-                        clone.classList.add('pdf-style');
-                        pdfCloneContainer.appendChild(clone);
-                        console.log(`Clon ${idx + 1} de contenedor ${index + 1} preparado para inspección:`, clone.outerHTML);
-                        setTimeout(() => {
-                            html2canvas(clone, {
-                                scale: 2,
-                                width: captureWidth
-                            }).then(canvas => {
-                                pdfCloneContainer.removeChild(clone);
-                                const imgData = canvas.toDataURL('image/jpeg');
-                                const imgWidth = captureWidth;
-                                const imgHeight = (canvas.height * imgWidth) / canvas.width;
-                                const marginLeft = (pdfWidth - imgWidth) / 2;
-                                if (currentPageHeight + imgHeight > pageHeight) {
-                                    pdf.addPage();
-                                    // Añadir el encabezado en la nueva página y ajustar currentPageHeight
-                                    this.addHeader(pdf, pdfWidth, headerHeight).then(() => {
-                                        currentPageHeight = headerHeight + spaceBelowHeader; // Restablecer la altura de la página actual
-                                        pdf.addImage(imgData, 'JPEG', marginLeft, currentPageHeight, imgWidth, imgHeight);
-                                        currentPageHeight += imgHeight;
-                                        resolveRow();
-                                    });
-                                } else {
-                                    pdf.addImage(imgData, 'JPEG', marginLeft, currentPageHeight, imgWidth, imgHeight);
-                                    currentPageHeight += imgHeight;
-                                    resolveRow();
-                                }
-                            });
-                        }, 0);
-                    });
-                });
-            });
-        });
-        promiseChain.then(() => {
-            pdf.save('test3.pdf');
-            console.log('PDF guardado correctamente.');
-        }).catch(err => {
-            console.error('Error al generar el PDF:', err);
-        });
+        for (const [index, container] of containers.entries()) {
+            if (index > 0) {
+                pdf.addPage();
+                currentPageHeight = headerHeight + spaceBelowHeader;
+                await this.addHeader(pdf, pdfWidth, headerHeight);
+            }
+            for (const [idx, heightItem] of Array.from(container.querySelectorAll('.pdf-item')).entries()) {
+                const clone = heightItem.cloneNode(true);
+                clone.classList.add('pdf-style');
+                pdfCloneContainer.appendChild(clone);
+                console.log(`Clon ${idx + 1} de contenedor ${index + 1} preparado para inspección`);
+                let additionalSpace = 0;
+                if (clone.classList.contains('accordion__button')) {
+                    additionalSpace = 60;
+                } else if (clone.classList.contains('accordion__finish')) {
+                    additionalSpace = 140;
+                } else if (clone.classList.contains('pdf-space-3')) {
+                    additionalSpace = 0;
+                }
+
+                const canvas = await html2canvas(clone, { scale: 1.5, width: captureWidth,  windowWidth: captureWidth });
+                pdfCloneContainer.removeChild(clone);
+                const imgData = canvas.toDataURL('image/jpeg');
+                const imgWidth = captureWidth;
+                const imgHeight = (canvas.height * imgWidth) / canvas.width;
+                const marginLeft = (pdfWidth - imgWidth) / 2;
+
+                if (currentPageHeight + imgHeight + additionalSpace > pageHeight) {
+                    pdf.addPage();
+                    await this.addHeader(pdf, pdfWidth, headerHeight);
+                    currentPageHeight = headerHeight + spaceBelowHeader;
+                }
+
+                pdf.addImage(imgData, 'JPEG', marginLeft, currentPageHeight, imgWidth, imgHeight);
+                currentPageHeight += imgHeight;
+                currentPageHeight += additionalSpace;
+            }
+        }
+
+        pdf.save('test4.pdf');
+        console.log('PDF guardado correctamente.');
     }
 
-    // Aplica estilos específicos para la generación del PDF
     applyPDFStyles() {
         const style = document.createElement('style');
         style.textContent = `
@@ -118,15 +114,14 @@ export default class CreatePDF extends BaseComponent {
         console.log('Estilos PDF aplicados:', style.textContent);
     }
 
-    // Función auxiliar para añadir el encabezado al PDF
-    addHeader(pdf, pdfWidth, headerHeight) {
-        return new Promise(resolveHeader => {
-            const img = new Image();
-            img.src = 'header.png';
+    async addHeader(pdf, pdfWidth, headerHeight) {
+        const img = new Image();
+        img.src = 'header.png';
+        await new Promise(resolve => {
             img.onload = () => {
                 pdf.addImage(img, 'PNG', 0, 0, pdfWidth, headerHeight);
                 console.log('Encabezado añadido');
-                resolveHeader();
+                resolve();
             };
         });
     }
